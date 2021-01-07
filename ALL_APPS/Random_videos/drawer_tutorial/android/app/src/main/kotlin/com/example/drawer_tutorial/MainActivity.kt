e5402edd03e657e3dcfccaae9f6fe5bc86af528b
Vicyos <felipendc10phoenix@gmail.com>
2021-01-07 20:07:50 -0300

    first commit

--- ALL_APPS/Random_videos/drawer_tutorial/android/app/src/main/kotlin/com/example/drawer_tutorial/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.drawer_tutorial
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- ALL_APPS/Random_videos/vicyos_listview/android/app/src/main/kotlin/com/example/vicyos_listview/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.vicyos_listview
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
