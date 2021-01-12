1022ec11ba7a5d9f602391465d4aca3bd4a39498
Vicyos <felipendc10phoenix@gmail.com>
2021-01-12 13:08:14 -0300

    Controlling the Focus!
    video 10

--- ALL_APPS/CODER/shop_app/lib/views/product_form_screen.dart
+++ ALL_APPS/CODER/shop_app/lib/views/product_form_screen.dart
@@ -9,6 +9,7 @@ class ProductFormScreen extends StatefulWidget {
 class _ProductFormScreenState extends State<ProductFormScreen> {
   @override
   Widget build(BuildContext context) {
+    final _priceFocusNode = FocusNode();
     return Scaffold(
       appBar: AppBar(
         title: Text('Formulário Produto'),
@@ -22,14 +23,24 @@ class _ProductFormScreenState extends State<ProductFormScreen> {
       body: Padding(
         padding: const EdgeInsets.all(15.0),
         child: Form(
-            child: ListView(
-          children: [
-            TextFormField(
-              decoration: InputDecoration(labelText: 'Título'),
-              textInputAction: TextInputAction.next,
-            ),
-          ],
-        )),
+          child: ListView(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Título'),
+                textInputAction: TextInputAction.next,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_priceFocusNode)
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Preço'),
+                textInputAction: TextInputAction.next,
+                focusNode: _priceFocusNode,
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
