6b788a9ebcc34301cc71c4c54cd8f6a1d333c462
felipendc <felipendc10@gmail.com>
2021-01-25 02:22:54 -0300

    up

---
+++ PYTHON/python_tipo_de_tados_primitivos.py
@@ -0,0 +1,29 @@
+# Tipos de dados:
+
+# String
+
+# Esse comando vai imprimir a caractere no índice 0
+# Esse metodo de puxar apenas um elemento em um certo índice/posição de uma string
+#  é chamado de: [Subscript / Subscripting]
+print("Hello"[0])
+
+
+# Números inteiros [Integer]:
+print(123 + 456)
+
+# No python eu posso substituir as virgulas por: _ "underscore"
+# Exemplo: 123_456_789 será interpretado como: 123456789 mas, pra nós humanos
+# saberemos que é a mesma coisa que 123,456,789
+print(123_456_789)
+
+
+# Números decimais [float]:
+print(float(3.14159))
+print(3.14159)
+
+
+# Booleano [Boolean]
+# Verdadeiro ou Falso
+
+# True
+# False
