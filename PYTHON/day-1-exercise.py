1b381bd0e5823e3104646df697ff1b20f98f3fd9
felipendc <felipendc10@gmail.com>
2021-01-25 01:48:14 -0300

    update

---
+++ PYTHON/day-1-exercise.py
@@ -0,0 +1,13 @@
+#Write your code below this line 👇
+# video 11
+
+username = input("What's your name? ")
+
+username_length = len(username) - username.count(' ')
+
+print(username_length)
+
+
+# Outro modo de imprimir o input na tela:
+# print( len( input("What's your name? ") ) )
+
