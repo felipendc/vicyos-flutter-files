dca1bc37c4d4281da0d2389663e2b2629cb0a016
Vicyos <felipendc10phoenix@gmail.com>
2021-01-26 02:09:09 -0300

    up

---
+++ PYTHON/2.5python_tipo_de_exercicios.py
@@ -0,0 +1,14 @@
+# Recebe os valores que o usuário inseriu:
+two_digit_number = input("Digite um valor com dois números: ")
+
+# Converte os dois números que o usuário inseriu para Inteiro
+# e soma o primeiro digito com o segundo e printa o resultado na tela.
+print(int(two_digit_number[0]) + int(two_digit_number[1]))
+
+
+
+# Exemplo: 
+# 55 = 10
+
+# index0 + index1 = 10
+# 5 + 5 = 10
