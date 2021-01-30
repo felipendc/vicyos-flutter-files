b6c377a880d9be1827814ae7dd88d1ec4b0cf92c
Vicyos <felipendc10phoenix@gmail.com>
2021-01-30 00:57:02 -0300

    Update 332_ multiplas_afirmacoes-montanha_russa.py

--- PYTHON/332_ multiplas_afirmacoes-montanha_russa.py
+++ PYTHON/332_ multiplas_afirmacoes-montanha_russa.py
@@ -7,18 +7,18 @@ if altura >= 120:
     idade = int(input("Qual é a sua idade? "))
     if idade < 12:
         conta = 5
-        print("Ingressos para crianças é R$5,00.")
+        print("Ingressos para crianças é R$ 5,00.")
     elif idade <= 18:
         conta = 7
-        print("Ingresso para juvenis são R$7.")
+        print("Ingresso para juvenis são R$ 7,00.")
     elif idade >= 45 and idade <= 55:
         print("Tudo vai ficar bem. Dê uma volta conosco!")
     else:
         conta = 12
-        print("Ingressos para adultos são R$12,00.")
+        print("Ingressos para adultos são R$ 12,00.")
 
-    wants_photo = input("Você quer tirar uma foto? S ou N. ")
-    if wants_photo == "S" or wants_photo == "s":
+    quer_foto = input("Você quer tirar uma foto? S ou N. ")
+    if quer_foto == "S" or quer_foto == "s":
         conta += 3
 
     print(f"A sua conta final é: R$ {conta},00")
