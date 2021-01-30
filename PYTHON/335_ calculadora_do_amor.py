6482de14bd56a574e7f5000d4e9b79d31e6decb6
Vicyos <felipendc10phoenix@gmail.com>
2021-01-30 13:49:33 -0300

    up

--- PYTHON/335_ calculadora_do_amor.py
+++ PYTHON/335_ calculadora_do_amor.py
@@ -1,10 +1,8 @@
 
-# print("Welcome to the Love Calculator!")
-# nome1 = input("Qual é o seu nome? \n")
-# nome2 = input("Qual é o nome dela / dele? \n")
+print("Welcome to the Love Calculator!")
+nome1 = input("Qual é o seu nome? \n")
+nome2 = input("Qual é o nome dela / dele? \n")
 
-nome1 = "Angela Yu"
-nome2 = "Jack Bauer"
 
 # Colocar as letras para minúsculas
 nome1_lower = nome1.lower()
@@ -17,26 +15,28 @@ nome_2_true = 0
 nome_1_love = 0
 nome_2_love = 0
 
-# Quantidades que as letras T R U E se repetem no nome 1
+# Quantidades que as letras T R U E se repetem no nome 1:
 nome_1_true += nome1_lower.count("t") + \
     nome1_lower.count("r") + \
     nome1_lower.count("u") + \
     nome1_lower.count("e")
 
-# Quantidades que as letras T R U E se repetem no nome 2
+# Quantidades que as letras T R U E se repetem no nome 2:
 nome_2_true += nome2_lower.count("t") + \
     nome2_lower.count("r") + \
     nome2_lower.count("u") + \
     nome2_lower.count("e")
 
 
-# Quantidades que as letras L O V E se repetem no nome 1
+# Quantidades que as letras L O V E se repetem no nome 1:
 nome_1_love += nome1_lower.count("l") + \
     nome1_lower.count("o") + \
     nome1_lower.count("v") + \
     nome1_lower.count("e")
 
 # Quantidades que as letras L O V E se repetem no nome 2
+# Adicional: o contra-barra está sendo usado aqui para eu poder pular a linha
+# mas, para o enterpretador:
 nome_2_love += nome2_lower.count("l") + \
     nome2_lower.count("o") + \
     nome2_lower.count("v") + \
@@ -47,10 +47,16 @@ total_de_love = nome_1_love + nome_2_love
 
 # JUNTAR O TOTAL DE "total_de_true" com o  "total_de_love" usando o F-"STRING" do Python
 # e transformando-o em um numero inteiro para eu poder fazer cálculos matemáticos com ele, caso for preciso.
-juntar_true_e_love = int(f"{total_de_true}{total_de_love}")
+total_true_e_love = int(f"{total_de_true}{total_de_love}")
 
 
-# Testar a saída código
-print(f"total de true: {total_de_true}")
-print(f"total de love: {total_de_love}")
-print(f"total de true e love: {juntar_true_e_love}")
+if total_true_e_love < 10 or total_true_e_love > 90:
+    print(
+        f"A sua potutação é: {total_true_e_love}, Vocês são como Coca-Cola e Mentos! hahaha.")
+
+elif total_true_e_love >= 40 and total_true_e_love <= 50:
+    print(
+        f"A sua potutação é: {total_true_e_love}, Vocês formam um ótimo casal!")
+else:
+    print(
+        f"Ah... Que pena! A sua potutação é: {total_true_e_love}. Boa sorte na procura do seu amor...")
