27f2566340e93a9f404940dc35d7cbd4fb6cd7e7
Vicyos <felipendc10phoenix@gmail.com>
2021-02-03 00:28:15 -0300

    Create 343_lista_quiz.py

---
+++ PYTHON/343_lista_quiz.py
@@ -0,0 +1,15 @@
+fruits = ["Strawberries", "Nectarines", "Apples",
+          "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+dirty_dozen = [fruits, vegetables]
+
+print(dirty_dozen[1][1])
+# Kale
+
+####################################
+# dirty_dozen[1][1]
+# dirty_dozen[index 1 da lista dirty_dozen][index 1 da lista vegetables]
+# dirty_dozen[vegetables]["Kale"]
+
+####################################
