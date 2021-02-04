88c44988ff6c5b4012de46f2a02bfdd22c7ff5ca
Vicyos <felipendc10phoenix@gmail.com>
2021-02-04 05:22:35 -0300

    Update 349_exercicio_for_loop-media-de-altura.py

--- PYTHON/349_exercicio_for_loop-media-de-altura.py
+++ PYTHON/349_exercicio_for_loop-media-de-altura.py
@@ -7,12 +7,12 @@ for n in range(0, len(altura_dos_estudantes)):
 soma_das_alturas = 0
 for altura in altura_dos_estudantes:
     soma_das_alturas += altura
-    print(f'Soma das alturas: {soma_das_alturas}')
+print(f'Soma das alturas: {soma_das_alturas}')
 
 quantidate_de_estudantes = 0
 for quantidate in altura_dos_estudantes:
     quantidate_de_estudantes += 1
-    print(f'Quantidades de estudantes: {quantidate_de_estudantes}')
+print(f'Quantidades de estudantes: {quantidate_de_estudantes}')
 
 
 media_da_altura = round(soma_das_alturas / quantidate_de_estudantes)
