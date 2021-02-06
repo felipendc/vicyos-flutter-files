672b1cda8d074f12db62d073375a53974ed11e49
Vicyos <felipendc10phoenix@gmail.com>
2021-02-06 15:13:28 -0300

    Update 367_jogo_da_forca_part1.py

--- PYTHON/367_jogo_da_forca_part1.py
+++ PYTHON/367_jogo_da_forca_part1.py
@@ -10,7 +10,7 @@ print(palavra_escolhida)
 
 
 # TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-letra_escolhida = input("Escolha uma letra: ")
+letra_escolhida = input("Escolha uma letra: ").lower()
 
 
 # TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
