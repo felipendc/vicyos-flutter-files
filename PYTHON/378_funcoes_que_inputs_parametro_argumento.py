5b05e8af6c098f19676b2cc741feef02ab0415b6
Vicyos <felipendc10phoenix@gmail.com>
2021-02-07 12:52:59 -0300

    Create 378_funcoes_que_inputs_parametro_argumento.py

---
+++ PYTHON/378_funcoes_que_inputs_parametro_argumento.py
@@ -0,0 +1,19 @@
+##############FUNÇÃO SIMOPLES ################
+def cumprimentar():
+    print('Olá.')
+    print('Eai, beleza?')
+    print('Olá, como você está?')
+
+
+cumprimentar()
+
+
+###########FUNÇÃO QUEP COM PARÂMETRO E QUE RECEBE UM ARGUMENTO ##########
+# O argumento é o que passa pelo parâmetro.
+def cumprimentar_com_nome(nome):
+    print('Olá, {nome}.')
+    print('Eai, {nome}, beleza?')
+    print('Oi, {nome}, como você está?')
+
+
+cumprimentar_com_nome('Felipe')
