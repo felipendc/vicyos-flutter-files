39719d59b54015e82b0729cd51ec508f358d1526
Vicyos <felipendc10phoenix@gmail.com>
2021-02-06 21:49:37 -0300

    Update 369_jogo_da_forca.py

--- PYTHON/369_jogo_da_forca.py
+++ PYTHON/369_jogo_da_forca.py
@@ -1,9 +1,71 @@
 import random
 import os
 
-##############################################################################
-############## PARTE QUE FICARÁ FÓRA DO WHILE LOOP: ##########################
-##############################################################################
+estagios_desenho = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+Irruh! Eu te avisei. Se você errasse, você já saberia o que ia acontecer.
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+Ops! você errou! Agora só restam: 1 vida. Se você errar, já sabem oque acontece, né?
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+Ops! você errou! Agora só restam: 2 vidas. Oh, Ouh!
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========
+Ops! você errou! Agora só restam: 3 vidas. Tá difícil, não?
+''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+Ops! você errou! Agora só restam: 4 vidas. Hmmm..!
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+Ops! você errou! Agora só restam: 5 vidas. Hahaha!
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+
+''']
+
 lista_de_palavras = ["aardvark", "baboon", "camel"]
 
 pegar_uma_palavra = random.randint(0, len(lista_de_palavras) - 1)
@@ -11,7 +73,11 @@ palavra_escolhida = lista_de_palavras[pegar_uma_palavra]
 
 # Guardará as palavras acertadas:
 display = []
-palavra_completada = []
+
+fim_do_jogo = False
+# Quantidades de vidas = 7 ao total.:
+vidas = 6
+
 
 for letra in palavra_escolhida:
     display.append("_")
@@ -20,13 +86,13 @@ for letra in palavra_escolhida:
 print(f'Pssst, a palavra escolhida foi: {palavra_escolhida}.')
 
 
-##############################################################################
-############## PARTE QUE FICARÁ DENTRO DO WHILE LOOP: ########################
-##############################################################################
-while not len(palavra_escolhida) == len(palavra_completada):
+###################  WHILE LOOP ##################
+while not fim_do_jogo:
 
     os.system('cls') or None  # Pra limpar o CMD do Windows!
     print(' '.join(display))
+    print(estagios_desenho[vidas])
+
     letra_escolhida = input("Escolha uma letra: ").lower()
 
     index_da_letra = 0
@@ -36,13 +102,19 @@ while not len(palavra_escolhida) == len(palavra_completada):
             display[index_da_letra] = letra
             index_da_letra += 1
 
-            # Add + 1 até ele conter o mesmo número
-            # de letra que a letra escolhida.
-            palavra_completada += letra
         else:
             index_da_letra += 1
 
-    os.system('cls') or None  # Pra limpar o CMD do Windows!
-    print(f'\nVocê escolheu a letra: {letra_escolhida}')
-    print("\nA palavra é: " + '"'+' '.join(display)+'"' + "\n")
-    print("Parabéns! Você venceu!")
+    if letra_escolhida not in palavra_escolhida:
+        vidas -= 1
+
+    if "_" not in display:
+        fim_do_jogo = True
+        print('\n\nVocê acertou todas seu MIZELÁÁÁ-VI! \n\n')
+
+    if vidas == 0:
+        fim_do_jogo = True
+
+        os.system('cls') or None  # Pra limpar o CMD do Windows!
+        print(estagios_desenho[vidas])
+        print('Ah, que peninha... parece que você perdeu! Hahaha.\n')
