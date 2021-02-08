3a458673d980667bddec34e4b55334e845a32e18
Vicyos <felipendc10phoenix@gmail.com>
2021-02-08 00:27:02 -0300

    up

--- PYTHON/382_numeros_primos.py
+++ PYTHON/382_numeros_primos.py
@@ -8,7 +8,7 @@ checar_numero = int(input('Checar número: '))
 
 def checar_primo_ou_composto(primo_ou_composto):
 
-    # Se houver apenas 2 dentro de "numero_de_divisoes" o número é primo!
+    # Se houver apenas o número 2 dentro de "numero_de_divisoes" o número é primo!
     numero_de_divisoes = 0
     for numeros in range(1, primo_ou_composto + 1):
         if primo_ou_composto % numeros == 0:
@@ -22,3 +22,25 @@ def checar_primo_ou_composto(primo_ou_composto):
 
 os.system('cls') or None
 checar_primo_ou_composto(checar_numero)
+
+
+############# Outra maneira de escrever o mesmo código: ####################
+
+# Write your code below this line 👇
+
+def prime_checker(number):
+    is_prime = True
+    for i in range(2, number):
+        if number % i == 0:
+            is_prime = False
+    if is_prime:
+        print("It's a prime number.")
+    else:
+        print("It's not a prime number.")
+
+# Write your code above this line 👆
+
+
+# Do NOT change any of the code below👇
+n = int(input("Check this number: "))
+prime_checker(number=n)
