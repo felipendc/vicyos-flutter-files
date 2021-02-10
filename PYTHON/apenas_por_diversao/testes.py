7d98a2f9b958d94b3153689ae2fafbb53a655dd4
felipendc <felipendc10@gmail.com>
2021-02-10 05:22:45 -0300

    Create testes.py

---
+++ PYTHON/apenas_por_diversao/testes.py
@@ -0,0 +1,12 @@
+# Como iterar entre listas!
+letras = ['á', 'à', 'â', 'ã', 'é', 'è', 'ê', 'í', 'ì', 'ó', 'ò', 'ô', 'ã', 'õ']
+simbolos = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+for elemento in (letras, simbolos, numeros):
+    print(elemento)
+
+
+# Como pular uma uma linha apenas no script.
+print('O Felipe \
+é foda')
