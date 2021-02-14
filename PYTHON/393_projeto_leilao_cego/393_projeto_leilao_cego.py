cffea2b1e1cd3530b5955daceb13de6560611ca1
Vicyos <felipendc10phoenix@gmail.com>
2021-02-14 15:55:49 -0300

    up

--- PYTHON/393_projeto_leilao_cego/393_projeto_leilao_cego.py
+++ PYTHON/393_projeto_leilao_cego/393_projeto_leilao_cego.py
@@ -1,15 +1,39 @@
 import os  # Pra limpar a tela do CMD
 import art
 
-print('Bem-vindo ao Leilão Cego')
+ofertadores = []
+maior_lance = []
 
 
-add_nome = input('Qual é o seu nome? ')
-add_lance = int(input('Qual é o seu lance? R$'))
-novo_apostador = add_nome = input(
-    "Alguém mais deseja ofertar? Digite 'sim' ou 'não'. ")
+def add_novo_ofertador(nome_do_ofertador, lance_do_ofertador):
+    ofertadores.append(
+        {
+            "ofertador": nome_do_ofertador,
+            "lance": int(lance_do_ofertador),
+        }
+    )
 
-ofertadores = {}
+
+# def ganhador():
 
 
-def add_novo_ofertador():
+# Part 1
+sem_mais_ofertadores = False
+while not sem_mais_ofertadores:
+
+    os.system('cls') or None
+    print(art.logo)
+    print('Bem-vindo ao Leilão Cego!')
+
+    add_nome = input('Qual é o seu nome? ').lower()
+    add_lance = int(input('Qual é o seu lance? R$'))
+
+    add_novo_ofertador(nome_do_ofertador=add_nome,
+                       lance_do_ofertador=add_lance)
+
+    novo_apostador = add_nome = input(
+        "Alguém mais deseja ofertar? Digite 'sim' ou 'não'. ").lower()
+    if novo_apostador == 'não' or novo_apostador == 'nao':
+        sem_mais_ofertadores = True
+
+print(ofertadores)
