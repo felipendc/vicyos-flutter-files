8d87ceec4ec44dbe87ae934d2f657b9700852af9
Vicyos <felipendc10phoenix@gmail.com>
2021-02-14 23:52:35 -0300

    Update 399_exercicio_mes_do_ano_bissexto.py

--- PYTHON/399_exercicio_mes_do_ano_bissexto.py
+++ PYTHON/399_exercicio_mes_do_ano_bissexto.py
@@ -28,3 +28,34 @@ ano = int(input("Digite um ano: "))
 mes = int(input("Digite um mês: "))
 dias = dias_no_mes(ano, mes)
 print(dias)
+
+
+########################################################
+###### OUTRA MANEIRA DE ESCREVER O MESMO CÓDIGO ########
+
+# def is_leap(year):
+#       if year % 4 == 0:
+#     if year % 100 == 0:
+#       if year % 400 == 0:
+#         return True
+#       else:
+#         return False
+#     else:
+#         return True
+#   else:
+#     return False
+
+# def days_in_month(year, month):
+#   month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+#   if month > 12 or month < 1:
+#     return "Invalid month entered."
+#   if month == 2 and is_leap(year):
+#     return 29
+#   return month_days[month - 1]
+
+
+# #Do NOT change any of the code below 👇
+# year = int(input("Enter a year: "))
+# month = int(input("Enter a month: "))
+# days = days_in_month(year, month)
+# print(days)
