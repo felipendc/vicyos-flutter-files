6e80b9b3fdd884584255c0385de0a8e1a60a1690
Vicyos <felipendc10phoenix@gmail.com>
2021-02-14 23:30:42 -0300

    up

---
+++ PYTHON/398_multiplos_retornos_de_valores.py
@@ -0,0 +1,31 @@
+# Funções com saídas
+def formatar_nome(p_nome, u_nome):
+    if p_nome == "" or u_nome == "":
+        return "Você não adicionou valores válidos."
+    p_nome_formatado = p_nome.title()
+    u_nome_formatado = u_nome.title()
+    f"Result: {p_nome_formatado} {u_nome_formatado}"
+
+
+# Salvando a saída em uma vareável
+nome_formatado = formatar_nome(
+    input("O seu primeiro: "), input("O seu último nome?: "))
+print(nome_formatado)
+# ou salvanddo a saída diretamente
+print(formatar_nome(input("Qual é o seu primeiro nome? "),
+                    input("Qual é o seu último nome? ")))
+
+# Funções com saída já usadas
+length = len(nome_formatado)
+
+# Retorna como um valor de saída
+
+
+def formatar_nome(p_nome, u_nome):
+    """Pegar o primeiro e o último nome e o formata
+    para retornar cada palavra com letras maiúsculas."""
+    if p_nome == "" or u_nome == "":
+        return "Você não adicionou valores válidos."
+    p_nome_formatado = p_nome.title()
+    u_nome_formatado = u_nome.title()
+    return f"Result: {p_nome_formatado} {u_nome_formatado}"
---
+++ PYTHON/399_exercicio_mes_do_ano_bissexto.py
@@ -0,0 +1,28 @@
+def ano_bissexto(ano):
+    if ano % 4 == 0:
+        if ano % 100 == 0:
+            if ano % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+
+def dias_no_mes(ano, mes):
+    dias_do_mes = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    mes_selecionado = mes - 1
+    ano_bissexto_resultado = ano_bissexto(ano)
+    if ano_bissexto_resultado:
+        dias_do_mes[1] = 29
+        return dias_do_mes[mes_selecionado]
+    else:
+        return dias_do_mes[mes_selecionado]
+
+
+ano = int(input("Digite um ano: "))
+mes = int(input("Digite um mês: "))
+dias = dias_no_mes(ano, mes)
+print(dias)
