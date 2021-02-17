204b53d1c95130479b60bffaff752f34d375285c
Vicyos <felipendc10phoenix@gmail.com>
2021-02-17 17:45:29 -0300

    up

---
+++ PYTHON/3100_docstrings_documentando_uma_funcao_.py
@@ -0,0 +1,9 @@
+# Lembresse = essa é a sintaxe da Docstring
+
+def uma_funcao_simples():
+    """Essa é uma Docstring, ela serve para documentar ou explicar oque a função que você criou faz. 
+    Toda vez que você chamar essa função, você só precisa encostar o mouse encima do nome da função e
+    ela vai falar oque essa função faz."""
+
+
+uma_funcao_simples()
