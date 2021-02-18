55697c96460f7761503ac1286e2601675eb27592
Vicyos <felipendc10phoenix@gmail.com>
2021-02-18 19:30:14 -0300

    bugs fixados

--- PYTHON/3101_calculadora/3101_main_calculadora.py
+++ PYTHON/3101_calculadora/3101_main_calculadora.py
@@ -30,37 +30,31 @@ numero1 = int(input('Qual é o primeiro número?: '))
 for chave in operadores:
     print(chave)
 
-
 simbolo_da_operacao = input('Escolha uma das operações acima: ')
 numero2 = int(input('Qual é o segundo número?: '))
 
 funcao_escolhida = operadores[simbolo_da_operacao]
-
 primeiro_resultado = funcao_escolhida(numero1, numero2)
 
 print(f"{numero1} {simbolo_da_operacao} {numero1} = {primeiro_resultado}")
 
-decisao_do_usuario = input(
-    f"Digite 'S' para continuar calculando com o {primeiro_resultado} ou digite 'N' para sair ").lower()
 
-if decisao_do_usuario == 's':
+resultado_anterior = primeiro_resultado
+parar_calculo = False
 
-    nao_continuar = False
-    while not nao_continuar:
+while not parar_calculo:
+    continuar_calculando = input(
+        f"Digite 'S' para continuar calculando com o {resultado_anterior} ou digite 'N' para sair ").lower()
 
-        reatualizar_resultado = primeiro_resultado
+    if continuar_calculando == 's':
         simbolo_da_operacao = input('Escolha escolha outra operação: ')
         numero3 = int(input('Qual é o próximo número?: '))
 
         funcao_escolhida = operadores[simbolo_da_operacao]
-        segundo_resultado = funcao_escolhida(reatualizar_resultado, numero3)
-        reatualizar_resultado = segundo_resultado
+        resultado_atual = funcao_escolhida(resultado_anterior, numero3)
 
         print(
-            f"{segundo_resultado} {simbolo_da_operacao} {numero3} = {segundo_resultado}")
-
-        decisao_do_usuario = input(
-            f"Digite 'S' para continuar calculando com o {reatualizar_resultado} ou digite 'N' para parar ").lower()
-
-        if decisao_do_usuario == 'n':
-            nao_continuar = True
+            f"{resultado_anterior} {simbolo_da_operacao} {numero3} = {resultado_atual}")
+        resultado_anterior = resultado_atual
+    else:
+        parar_calculo = True
