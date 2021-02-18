5d8927e136c472985e191306c72b843e5db0f86d
Vicyos <felipendc10phoenix@gmail.com>
2021-02-18 17:50:34 -0300

    Update 3101_main_calculadora.py

--- PYTHON/3101_calculadora/3101_main_calculadora.py
+++ PYTHON/3101_calculadora/3101_main_calculadora.py
@@ -30,11 +30,19 @@ numero1 = int(input('Qual é o primeiro número?: '))
 for chave in operadores:
     print(chave)
 
+
 simbolo_da_operacao = input('Escolha uma das operações acima: ')
 numero2 = int(input('Qual é o segundo número?: '))
 
 funcao_escolhida = operadores[simbolo_da_operacao]
 
-resultado = funcao_escolhida(numero1, numero2)
+primeiro_resultado = funcao_escolhida(numero1, numero2)
 
-print(f"{numero1} {simbolo_da_operacao} {numero1} = {resultado}")
+print(f"{numero1} {simbolo_da_operacao} {numero1} = {primeiro_resultado}")
+
+simbolo_da_operacao = input('Escolha escolha outra operação: ')
+numero3 = int(input('Qual é o próximo número?: '))
+
+funcao_escolhida = operadores[simbolo_da_operacao]
+segundo_resultado = funcao_escolhida(primeiro_resultado, numero3)
+print(f"{primeiro_resultado} {simbolo_da_operacao} {numero3} = {segundo_resultado}")
