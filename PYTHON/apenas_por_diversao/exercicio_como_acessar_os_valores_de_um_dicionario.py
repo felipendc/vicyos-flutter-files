b1d073470271389bec2ad27ab1f2937d5f024b7a
Vicyos <felipendc10phoenix@gmail.com>
2021-02-18 20:45:28 -0300

    Update exercicio_como_acessar_os_valores_de_um_dicionario.py

--- PYTHON/apenas_por_diversao/exercicio_como_acessar_os_valores_de_um_dicionario.py
+++ PYTHON/apenas_por_diversao/exercicio_como_acessar_os_valores_de_um_dicionario.py
@@ -1,16 +1,21 @@
 teste = {
-    "name": "Redmi K30/Poco X2",
+    "name": "Redmi K20 Pro/Mi 9T Pro",
     "brand": "Xiaomi",
-    "codename": "phoenix",
+    "codename": "raphael",
     "supported_versions": [
         {
+            "version_code": "eleven",
+            "xda_thread": "",
+            "stable": "false"
+        },
+        {
             "version_code": "ten",
-            "xda_thread": "https://forum.xda-developers.com/poco-x2/development/rom-pixel-experience-t4073897",
+            "xda_thread": "https://forum.xda-developers.com/k20-pro/development/rom-pixel-experience-t4014141",
             "deprecated": "true"
         },
         {
             "version_code": "ten_plus",
-            "xda_thread": "https://forum.xda-developers.com/poco-x2/development/rom-pixel-experience-t4073897",
+            "xda_thread": "https://forum.xda-developers.com/k20-pro/development/rom-pixel-experience-t4014141",
             "deprecated": "true"
         }
     ]
@@ -26,9 +31,14 @@ print(teste["supported_versions"])
 # Printar o valor da cada chave do dicionário que está dentro de uma lista na posisão 0:
 print(teste["supported_versions"][0]["version_code"])
 print(teste["supported_versions"][0]["xda_thread"])
-print(teste["supported_versions"][0]["deprecated"])
+print(teste["supported_versions"][0]["stable"])
 
 # Printar o valor da cada chave do dicionário que está dentro de uma lista na posisão 1:
+print(teste["supported_versions"][1]["version_code"])
+print(teste["supported_versions"][1]["xda_thread"])
+print(teste["supported_versions"][1]["deprecated"])
+
+# Printar o valor da cada chave do dicionário que está dentro de uma lista na posisão 2:
 print(teste["supported_versions"][1]["version_code"])
 print(teste["supported_versions"][1]["xda_thread"])
 print(teste["supported_versions"][1]["deprecated"])
