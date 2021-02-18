1176f3907001a8632cc9c2a1fea3c19a814b00c0
Vicyos <felipendc10phoenix@gmail.com>
2021-02-18 18:35:52 -0300

    ainda em desenvolvimento

--- PYTHON/3101_calculadora/3101_main_calculadora.py
+++ PYTHON/3101_calculadora/3101_main_calculadora.py
@@ -40,9 +40,27 @@ primeiro_resultado = funcao_escolhida(numero1, numero2)
 
 print(f"{numero1} {simbolo_da_operacao} {numero1} = {primeiro_resultado}")
 
-simbolo_da_operacao = input('Escolha escolha outra operação: ')
-numero3 = int(input('Qual é o próximo número?: '))
+decisao_do_usuario = input(
+    f"Digite 'S' para continuar calculando com o {primeiro_resultado} ou digite 'N' para sair ").lower()
 
-funcao_escolhida = operadores[simbolo_da_operacao]
-segundo_resultado = funcao_escolhida(primeiro_resultado, numero3)
-print(f"{primeiro_resultado} {simbolo_da_operacao} {numero3} = {segundo_resultado}")
+if decisao_do_usuario == 's':
+
+    nao_continuar = False
+    while not nao_continuar:
+
+        reatualizar_resultado = primeiro_resultado
+        simbolo_da_operacao = input('Escolha escolha outra operação: ')
+        numero3 = int(input('Qual é o próximo número?: '))
+
+        funcao_escolhida = operadores[simbolo_da_operacao]
+        segundo_resultado = funcao_escolhida(reatualizar_resultado, numero3)
+        reatualizar_resultado = segundo_resultado
+
+        print(
+            f"{segundo_resultado} {simbolo_da_operacao} {numero3} = {segundo_resultado}")
+
+        decisao_do_usuario = input(
+            f"Digite 'S' para continuar calculando com o {reatualizar_resultado} ou digite 'N' para parar ").lower()
+
+        if decisao_do_usuario == 'n':
+            nao_continuar = True
