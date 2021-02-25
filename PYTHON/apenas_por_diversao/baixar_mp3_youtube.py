49c24e003181f099bc84c3ced20dc110fac274f4
Vicyos <felipendc10phoenix@gmail.com>
2021-02-24 23:23:38 -0300

    up

--- PYTHON/apenas_por_diversao/baixar_mp3_youtube.py
+++ PYTHON/apenas_por_diversao/baixar_mp3_youtube.py
@@ -4,12 +4,9 @@ from moviepy.editor import *
 
 def baixar_mp3():
     url = input('Insira o link do vídeo: ')
-    saida = input('Qual o formato que você quer? (wav/mp3): ')
-
     print('Convertendo...')
-
     mp4 = YouTube(url).streams.get_highest_resolution().download()
-    mp3 = mp4.split('.mp4', 1)[0] + f".{saida}"
+    mp3 = mp4.split('.mp4', 1)[0] + f".mp3"
 
     video_clip = VideoFileClip(mp4)
     audio_clip = video_clip.audio
