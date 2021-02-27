871f7a4f3f9eed21e8e1876b5a12f9bdf830aad1
Vicyos <felipendc10phoenix@gmail.com>
2021-02-26 21:55:39 -0300

    up

---
+++ PYTHON/apenas_por_diversao/project_manager.py
@@ -0,0 +1,17 @@
+
+# y = f(x)
+
+
+# X: A entrada ou as entradas, fatores ou o que for necessário para obter o resultado
+# (pode haver mais de um x possível).
+def f(x):
+
+    # F: a função ou processo que pegará as entradas e as transformará no resultado desejado.
+    return x + 1
+
+
+# Y: O resultado ou resultados que você deseja.
+y = f(2)
+
+# Imprimir o "Y", o resultado ou resultados que você deseja.
+print(y)
