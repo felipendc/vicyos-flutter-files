8ad52df442e0bd92e3258f88181f08f4fcc860a1
Vicyos <felipendc10phoenix@gmail.com>
2021-05-05 14:19:35 -0300

    12

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,27 @@
+/* 
 let js = "incrível";
-if (js === "incrível") alert("O JavaScript é muito DIVERTIDO!!!");
-40 + 8 + 23 - 10;
+if (js === "") alert("O JavaScript é muito DIVERTIDO!!!");
+
 console.log(40 + 8 + 23 - 10);
+console.log("Felipe");
+console.log(23);
+
+// Barra dupla é usado para comentar uma parte do código.
+let primeiroNome = "Felipe";
+let primeiro_nome = "Felipe";
+console.log(primeiroNome);
+*/
+
+let javascriptEDivertido = true;
+
+console.log(javascriptEDivertido);
+
+console.log(typeof true);
+console.log(typeof javascriptEDivertido);
+console.log(typeof 23);
+console.log(typeof "Java");
+
+// Dynamic type é quando trocamos o valor
+// de uma variável em uma outra parte do código.
+javascriptEDivertido = "SIM!";
+console.log(javascriptEDivertido);
